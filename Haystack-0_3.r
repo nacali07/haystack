ba06e66381acf3ebb092a2195691ddca2387c9b8
Bill West <williamjwest@gmail.com>
2014-01-01 23:12:37 -0500

    Added restore/save functionality.

--- Haystack-0_3.r
+++ Haystack-0_3.r
@@ -1,95 +1,124 @@
-## Classes useful in statistical modeling in finance institutions
+## These classes are designed to improve the efficiency of modeling in R within
+## finance institutions.  The improvements these offer over my existing process
+## are several:
+## 1. Datasets and models are objects that can be passed by reference to
+##    functions and methods
+## 2. Each verion of a model exists and an object which can be easily copied
+##    into a new object and modified slightly.  
 ## This utilizes the recently developed reference classes.
 ## Im trying to design this to mimic the real life financial and econometric
-## modeling process.  
+## modeling process.
+## 0. Create Haystack instance.  Set appropriate parameters
 ## 1. Read data into the Dataset reference class (RC) object
-## 2. 
+## 2. Create model instance, set parameters, and associate data
+## 3. Run all appropriate methods to do your work
+## 4. Save model and dataset to a location
 ## Bill West: 2013-12-30 <williamjwest@gmail.com>
 
 
 errorInvalidDataset<-function(dsnames){
   stop(paste(
     "Please specify valid data name:"
-    ,dsnames)                                   
+    ,dsnames))                                   
 }
 
 HaystackModel<-setRefClass('HaystackModel'
-                   ,fields=c(
-                       x_transformer = "function"
-                       y_transformer = "function"
-                     , formula = "ANY"   
-                     , scorer = "function"
-                     , validator = "function"
-                     , estimator = "function"   
-                     , model = "ANY"
-                     , name = "character"   
-                     , datasets = 'list'   
-                    ),
-                    methods = list(
-                      y_transform = function(x){
-                           if(! x %in% names(datasets)){
-                             errorInvalidDataset(names(datasets))                                 
-                           } else {
-                             y_transformer(.self$datasets[[x]])
-                           }    
-                      },
-                      x_transform = function(x){
-                           if(! x %in% names(datasets)){
-                             errorInvalidDataset(names(datasets))                                 
-                           } else {
-                             x_transformer(.self$datasets[[x]])
-                           }    
-                      },
-                      score = function(x){
-                           if(! x %in% names(datasets)){
-                             errorInvalidDataset(names(datasets))                                 
-                           } else {
-                             pname<-paste("pred_",name,sep="")
-                             .self$datasets[[x]]$data[,pname] <<- scorer(.self$datasets[[x]],.self$model)
-                           }    
-                      },                
-                      validate = function(x){
-                           if(! x %in% names(datasets)){
-                             errorInvalidDataset(names(datasets))                                                                   
-                           } else {
-                             validator(.self$datasets[[x]])
-                           }    
-                      },                
-                      vts = function(x){
-                           validate(x,name)
-                           transform(x,name)
-                           score(x,name)   
-                      },
-                      getpred = function(x,key=NULL,names=NULL){
-                           if(! x %in% names(datasets)){
-                             errorInvalidDataset(names(datasets))                                                                   
-                           } else {                                  
-                             cols<-c(key,names)
-                             if(is.null(cols)){
-                               .self$datasets[[x]]$data[,grep("pred_",colnames(.self$datasets[[x]]$data),value=T)]
-                             } else if(key %in% colnames(.self$datasets[[x]]$data) & is.null(names)){
-                               .self$datasets[[x]]$data[,c(key,grep("pred_",colnames(data),value=T))]
-                             } else if(all(cols %in% colnames(.self$datasets[[x]]$data))){
-                               .self$datasets[[x]]$data[,cols]
-                             } else {
-                               for(i in cols){
-                                 if(! cols[i]  %in% colnames(.self$datasets[[x]]$data)){
-                                   print(paste(cols[i],"not in dataset."))
-                                 }
-                               }
-                             }
-                        },
-                        viewperformance = function(){
-                             
-                        },
-                        estimate        = function(){
-                             model <<- estimator(formula,x=devdata,...)
-                        }
-                       ))
+     ,fields=c(
+         x_transformer = "function"
+       , y_transformer = "function"
+       , formula = "ANY"   
+       , scorer = "function"
+       , validator = "function"
+       , estimator = "function"   
+       , model = "ANY"
+       , name = "character"   
+       , datasets = 'list'
+       , homedir = "character"   
+      ),
+      methods = list(
+        saveRds = function(){
+            if(is.null(homedir)){
+                stop("Please set homedir before saving: e.g. x$setHomeDir('~/path')")
+            }
+            if (is.null(name)){
+                stop("Please set name before saving: e.g. x$setName('data76') or x$name<-'data76'")
+            }
+
+            newfile<-paste(homedir,"models/",name,".rds",sep="")
+            if(file.exists(newfile)){
+                response<-ask(newfile,' exists. Overwrite? (y/n) > ')
+                if(tolower(response)!='y'){
+                    stop("saveRDS aborted.")
+                }
+            }   
+            saveRDS(.self,file=newfile)
+        },        
+         y_transform = function(x){
+             if(! x %in% names(datasets)){
+               errorInvalidDataset(names(datasets))                                 
+             } else {
+               y_transformer(.self$datasets[[x]])
+             }    
+         },
+         x_transform = function(x){
+             if(! x %in% names(datasets)){
+               errorInvalidDataset(names(datasets))                                 
+             } else {
+               x_transformer(.self$datasets[[x]])
+             }    
+         },
+         score = function(x){
+             if(! x %in% names(datasets)){
+               errorInvalidDataset(names(datasets))                                 
+             } else {
+               pname<-paste("pred_",name,sep="")
+               .self$datasets[[x]]$data[,pname] <<- scorer(.self$datasets[[x]],.self$model)
+             }    
+         },                
+         validate = function(x){
+             if(! x %in% names(datasets)){
+               errorInvalidDataset(names(datasets))                                                                   
+             } else {
+               validator(.self$datasets[[x]])
+             }    
+         },                
+         vts = function(x){
+             validate(x,name)
+             transform(x,name)
+             score(x,name)   
+         },
+         getpred = function(x,key=NULL,names=NULL){
+             if(! x %in% names(datasets)){
+               errorInvalidDataset(names(datasets))                                                                   
+             } else {                                  
+               cols<-c(key,names)
+               if(is.null(cols)){
+                 .self$datasets[[x]]$data[,grep("pred_",colnames(.self$datasets[[x]]$data),value=T)]
+               } else if(key %in% colnames(.self$datasets[[x]]$data) & is.null(names)){
+                 .self$datasets[[x]]$data[,c(key,grep("pred_",colnames(data),value=T))]
+               } else if(all(cols %in% colnames(.self$datasets[[x]]$data))){
+                 .self$datasets[[x]]$data[,cols]
+               } else {
+                 for(i in cols){
+                   if(! cols[i]  %in% colnames(.self$datasets[[x]]$data)){
+                     print(paste(cols[i],"not in dataset."))
+                   }
+                 }
+               }
+            }
+         },
+         viewperformance = function(){
+               
+         },
+         estimate        = function(){
+               model <<- estimator(formula,x=devdata,...)
+         }
+      ))
 
 HaystackQuery<-setRefClass('HaystackQuery'
-                   ,fields=c(con="function",query="character",execution="function")
-                  )
+     ,fields=c(con="function",query="character",execution="function")
+     )
+
 HaystackJsonQuery<-setRefClass('HaystackJsonQuery'
                        ,contains="HaystackQuery"
                        ,methods=list(
@@ -112,50 +141,96 @@ HaystackMySQLQuery<-setRefClass('HaystackMySQLQuery'
                             }
                           }))
 
+
+RestoreHaystack<-function(name,homedir){
+   stopifnot(!is.null(name))
+   stopifnot(!is.null(homedir))
+
+   homedir <<- paste(gsub("\\/$","",homedir),"/",sep="")
+   filename<-paste(homedir,"datasets/",name,".rds",sep="")
+   return(readRDS(filename))
+}
+
+RestoreHaystackModel<-function(name,homedir){
+   stopifnot(!is.null(name))
+   stopifnot(!is.null(homedir))
+
+   homedir <<- paste(gsub("\\/$","",homedir),"/",sep="")
+   filename<-paste(homedir,"models/",name,".rds",sep="")
+   return(readRDS(filename))
+}
+
 Haystack<-setRefClass('Haystack'
-                     ,fields=c(  data          = "data.frame"  
-                               , query         = "ANY"    #returns data.frame
-                               , jsonparser    = "function"   
-                               , snapshot_date = "Date"
-                               , notes         = "character")
-                     ,methods=list(
-                       addNote = function(x){
-                          notes <<- c(notes, paste(now(),":",x))
-                       },
-                       readJson = function(){
-                             data <<- jsonParser$execution()
-                             addNote("Data obtained from json")
-                       },
-                       readDb = function(){
-                             data <<- query$execution()
-                             addNote("Data obtained from db.")
-                       },
-                       setJsonParser  = function(x){
-                         jsonParser <<-x$copy()
-                         addNote("JSON Parser set.")
-                       },
-                       setDbQuery = function(x){
-                         query <<-x$copy()
-                         addNote("Query Set.")
-                       },
-                       getpred = function(key=NULL,names=NULL){
-                         cols<-c(key,names)
-                         if(is.null(cols)){
-                           data[,grep("pred_",colnames(data),value=T)]
-                         } else if(key %in% colnames(data) & is.null(names)){
-                           data[,c(key,grep("pred_",colnames(data),value=T))]
-                         } else if(all(cols %in% colnames(data))){
-                           data[,cols]
-                         } else {
-                           for(i in cols){
-                             if(! cols[i]  %in% colnames(data)){
-                               print(paste(cols[i],"not in dataset."))
-                             }
-                           }
-                         }
-                       },
-                       summaryStats=function(){
-                         summary(data)  
-                       }
-                       ))
+      ,fields=c(
+            data          = "data.frame"  
+          , query         = "ANY"    #returns data.frame
+          , jsonparser    = "function"   
+          , snapshot_date = "Date"
+          , notes         = "character"
+          , homedir       = "character"
+          , name          = "character"
+          , description   = "character")
+      ,methods=list(
+        setName = function(x){
+            name <<- x
+        },            
+        setHomeDir = function(x){
+            homedir <<- paste(gsub("\\/$","",x),"/",sep="")
+        },
+        saveRds = function(){
+            if(is.null(homedir)){
+                stop("Please set homedir before saving: e.g. x$setHomeDir('~/path')")
+            }
+            if (is.null(name)){
+                stop("Please set name before saving: e.g. x$setName('data76') or x$name<-'data76'")
+            }
+
+            newfile<-paste(homedir,"datasets/",name,".rds",sep="")
+            if(file.exists(newfile)){
+                response<-ask(newfile,' exists. Overwrite? (y/n) > ')
+                if(tolower(response)!='y'){
+                    stop("saveRDS aborted.")
+                }
+            }   
+            saveRDS(.self,file=newfile)
+        },        
+        addNote = function(x){
+           notes <<- c(notes, paste(now(),":",x))
+        },
+        readJson = function(){
+              data <<- jsonParser$execution()
+              addNote("Data obtained from json")
+        },
+        readDb = function(){
+              data <<- query$execution()
+              addNote("Data obtained from db.")
+        },
+        setJsonParser  = function(x){
+          jsonParser <<-x$copy()
+          addNote("JSON Parser set.")
+        },
+        setDbQuery = function(x){
+          query <<-x$copy()
+          addNote("Query Set.")
+        },
+        getpred = function(key=NULL,names=NULL){
+          cols<-c(key,names)
+          if(is.null(cols)){
+            data[,grep("pred_",colnames(data),value=T)]
+          } else if(key %in% colnames(data) & is.null(names)){
+            data[,c(key,grep("pred_",colnames(data),value=T))]
+          } else if(all(cols %in% colnames(data))){
+            data[,cols]
+          } else {
+            for(i in cols){
+              if(! cols[i]  %in% colnames(data)){
+                  print(paste(cols[i],"not in dataset."))
+              }
+            }
+          }
+        },
+        summaryStats=function(){
+          summary(data)  
+        }
+    ))
 
