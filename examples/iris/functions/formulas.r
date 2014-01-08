76e6ea223d2e32f9f40b3a1f0ab12ee2ad9a365a
Callixte <nacali07@yahoo.fr>
2014-01-07 22:44:24 -0500

    same predictors are used to model isvirginica

--- examples/iris/functions/formulas.r
+++ examples/iris/functions/formulas.r
@@ -4,3 +4,6 @@ irisLogitFormula<-formula(
 irisRfFormula<-formula(
   isVirginica ~ Petal_Width + Petal_Length + Sepal_Length + Sepal_Width
 )
+" In both logistic and R factor fitting models formulae isVirginica is modeled as petal_Width, Petal_Length
+  Sepal_Length, and Sepal_Width (i.e these are predictors used to model isVirginica)
+"
